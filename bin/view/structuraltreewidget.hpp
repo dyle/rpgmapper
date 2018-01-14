d58be24de32de61cbbdbbb956727e6695f35befc
Oliver Maurhart <oliver.maurhart@ait.ac.at>
2018-01-14 11:25:42 +0100

    first sketch of a own structural tree widget

---
+++ bin/view/structuraltreewidget.hpp
@@ -0,0 +1,67 @@
+/*
+ * structuraltreewidget.hpp
+ *
+ * Copyright (C) 2015-2018 Oliver Maurhart, <dyle71@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef VIEW_STRUCTURALTREEWIDGET_HPP
+#define VIEW_STRUCTURALTREEWIDGET_HPP
+
+
+// ------------------------------------------------------------
+// incs
+
+// Qt
+#include <QTreeWidget>
+
+
+// ------------------------------------------------------------
+// decl
+
+
+namespace rpgmapper {
+namespace view {
+
+
+/**
+ * A QTreeWidget which displays the Atlas structure.
+ */
+class StructuralTreeWidget : public QTreeWidget {
+
+
+    Q_OBJECT
+
+
+public:
+
+
+    /**
+     * Ctor
+     *
+     * @param   cParent     parent widget
+     */
+    StructuralTreeWidget(QWidget * cParent = nullptr);
+
+
+};
+
+
+}
+}
+
+
+#endif

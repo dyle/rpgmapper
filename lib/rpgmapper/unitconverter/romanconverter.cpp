a27384bf480abcaf9095edea0fa02d098b7afd33
Oliver Maurhart <oliver.maurhart@ait.ac.at>
2018-02-02 14:28:38 +0100

    adds first sketch of roman converter

--- lib/rpgmapper/unitconverter/romanconverter.cpp
+++ lib/rpgmapper/unitconverter/romanconverter.cpp
@@ -27,6 +27,29 @@ using namespace rpgmapper::model;
 
 
 // ------------------------------------------------------------
+// decl
+
+
+/**
+ * Convert an integer to roman.
+ *
+ * @param   nValue      the value to convert
+ * @return  the string holding the roman value
+ */
+static QString convertRoman(int nValue);
+
+
+// ------------------------------------------------------------
+// vars
+
+
+/**
+ * Lookup table.
+ */
+static std::map<int, QString> g_cLookupValues;            /**< Known cached values. */
+
+
+// ------------------------------------------------------------
 // code
 
 
@@ -37,5 +60,93 @@ using namespace rpgmapper::model;
  * @return  A string holding the user value
  */
 QString RomanConverter::convert(int nValue) const {
-    return QString::number(nValue);
+
+    if (g_cLookupValues.empty()) {
+        for (int n = 0; n <= 100; ++n) {
+            g_cLookupValues[n] = convertRoman(n);
+        }
+    }
+
+    auto iterLookup = g_cLookupValues.find(nValue);
+    if (iterLookup != g_cLookupValues.end()) {
+        return (*iterLookup).second;
+    }
+
+    auto res = convertRoman(nValue);
+    g_cLookupValues[nValue] = res;
+
+    return res;
+}
+
+
+/**
+ * Convert an integer to roman.
+ *
+ * @param   nValue      the value to convert
+ * @return  the string holding the roman value
+ */
+QString convertRoman(int nValue) {
+
+    if (nValue < 0) {
+        return QString("-") + convertRoman(-nValue);
+    }
+    if (nValue == 0) {
+        return QString("O");
+    }
+
+    QString res = "";
+
+    while (nValue >= 1000) {
+        res += "M";
+        nValue -= 1000;
+    }
+    while (nValue >= 900) {
+        res += "CM";
+        nValue -= 900;
+    }
+    while (nValue >= 500) {
+        res += "D";
+        nValue -= 500;
+    }
+    while (nValue >= 400) {
+        res += "CD";
+        nValue -= 400;
+    }
+    while (nValue >= 100) {
+        res += "C";
+        nValue -= 100;
+    }
+    while (nValue >= 90) {
+        res += "XC";
+        nValue -= 90;
+    }
+    while (nValue >= 50) {
+        res += "L";
+        nValue -= 50;
+    }
+    while (nValue >= 40) {
+        res += "XL";
+        nValue -= 40;
+    }
+    while (nValue >= 10) {
+        res += "X";
+        nValue -= 10;
+    }
+    while (nValue >= 9) {
+        res += "IX";
+        nValue -= 9;
+    }
+    while (nValue >= 5) {
+        res += "V";
+        nValue -= 5;
+    }
+    while (nValue >= 4) {
+        res += "IV";
+        nValue -= 4;
+    }
+    while (nValue >= 1) {
+        res += "I";
+        nValue -= 1;
+    }
+    return res;
 }

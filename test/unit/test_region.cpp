c0a7e5828d3365d2af647ce2b25fc6a4487df2a8
Oliver Maurhart <oliver.maurhart@ait.ac.at>
2018-02-06 20:09:06 +0100

    adds region tests

---
+++ test/unit/test_region.cpp
@@ -0,0 +1,48 @@
+/*
+ * This file is part of rpgmapper.
+ * See the LICENSE file for the software license.
+ * (C) Copyright 2018, Oliver Maurhart, dyle71@gmail.com
+ */
+
+#include <tuple>
+
+#include <gtest/gtest.h>
+
+#include <rpgmapper/region.hpp>
+
+using namespace rpgmapper::model;
+
+
+TEST(RegionTest, CreateRegionIsValid) {
+    Region region{"foo"};
+    ASSERT_TRUE(region.isValid());
+}
+
+
+TEST(RegionTest, CreateRegionPointerIsValid) {
+    RegionPointer region{new Region{"foo"}};
+    EXPECT_NE(region.data(), nullptr);
+    EXPECT_TRUE(region->isValid());
+}
+
+
+TEST(RegionTest, CreateInvalidRegionIsNotValid) {
+    EXPECT_FALSE(InvalidRegion{}.isValid());
+}
+
+
+TEST(RegionTest, CreateInvalidRegionPointerIsNotValid) {
+    RegionPointer region{new InvalidRegion{}};
+    EXPECT_NE(region.data(), nullptr);
+    EXPECT_FALSE(region->isValid());
+}
+
+
+TEST(RegionTest, RegionGetAndSetName) {
+    Region region{"foo"};
+    EXPECT_EQ(region.getName().toStdString(), "foo");
+    region.setName("bar");
+    EXPECT_EQ(region.getName().toStdString(), "bar");
+}
+
+

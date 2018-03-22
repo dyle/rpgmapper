6658316899c9d78b51385776b1a6f6db15f7de7e
dyle <dyle71@gmail.com>
2018-03-22 09:33:25 +0100

    adds reource testing

---
+++ test/unit/test_resource.cpp
@@ -0,0 +1,34 @@
+/*
+ * This file is part of rpgmapper.
+ * See the LICENSE file for the software license.
+ * (C) Copyright 2018, Oliver Maurhart, dyle71@gmail.com
+ */
+
+
+#include <gtest/gtest.h>
+
+#include <rpgmapper/atlas_io.hpp>
+
+using namespace rpgmapper::model;
+
+
+TEST(ResoucrceDB, InsertResource) {
+
+    auto resources = ResourceDBPointer{new ResourceDB};
+
+    auto data = QByteArray::fromHex("0102030405060708090a0b0c0d0e0f10");
+    resources->addResource("data", data);
+
+    EXPECT_EQ(resources->getResources().size(), 1);
+
+    auto pair = resources->getResources().find("data");
+    bool found = (pair != resources->getResources().end());
+    EXPECT_FALSE(found);
+
+    pair = resources->getResources().find(Resource::getHash(data));
+    found = (pair != resources->getResources().end());
+    EXPECT_TRUE(found);
+    EXPECT_EQ((*pair).second->getName().toStdString(), "data");
+    EXPECT_EQ((*pair).second->getHash(), Resource::getHash(data));
+    EXPECT_EQ((*pair).second->getData().toHex().toStdString(), "0102030405060708090a0b0c0d0e0f10");
+}

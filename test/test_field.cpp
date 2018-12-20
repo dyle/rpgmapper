4f44e853dc91a66f994707f69c8a925700576c1f
dyle <dyle71@gmail.com>
2018-12-20 08:36:27 +0100

    fixes unit tests for fields

--- test/test_field.cpp
+++ test/test_field.cpp
@@ -26,9 +26,9 @@ TEST(FieldTest, FieldIndexInstances) {
     EXPECT_EQ(Field(0, 0).getIndex(), 0);
     EXPECT_EQ(Field(10, 0).getIndex(), 10);
     EXPECT_EQ(Field(999, 0).getIndex(), 999);
-    EXPECT_EQ(Field(0, 1).getIndex(), 1000);
-    EXPECT_EQ(Field(1, 1).getIndex(), 1001);
-    EXPECT_EQ(Field(10, 10).getIndex(), 10010);
+    EXPECT_EQ(Field(0, 1).getIndex(), 1000000);
+    EXPECT_EQ(Field(1, 1).getIndex(), 1000001);
+    EXPECT_EQ(Field(10, 10).getIndex(), 10000010);
 }
 
 
@@ -37,9 +37,9 @@ TEST(FieldTest, FieldIndexPoints) {
     EXPECT_EQ(Field::getIndex(QPoint(0, 0)), 0);
     EXPECT_EQ(Field::getIndex(QPoint(10, 0)), 10);
     EXPECT_EQ(Field::getIndex(QPoint(999, 0)), 999);
-    EXPECT_EQ(Field::getIndex(QPoint(0, 1)), 1000);
-    EXPECT_EQ(Field::getIndex(QPoint(1, 1)), 1001);
-    EXPECT_EQ(Field::getIndex(QPoint(10, 10)), 10010);
+    EXPECT_EQ(Field::getIndex(QPoint(0, 1)), 1000000);
+    EXPECT_EQ(Field::getIndex(QPoint(1, 1)), 1000001);
+    EXPECT_EQ(Field::getIndex(QPoint(10, 10)), 10000010);
 }
 
 
@@ -48,9 +48,9 @@ TEST(FieldTest, FieldIndexValues) {
     EXPECT_EQ(Field::getIndex(0, 0), 0);
     EXPECT_EQ(Field::getIndex(10, 0), 10);
     EXPECT_EQ(Field::getIndex(999, 0), 999);
-    EXPECT_EQ(Field::getIndex(0, 1), 1000);
-    EXPECT_EQ(Field::getIndex(1, 1), 1001);
-    EXPECT_EQ(Field::getIndex(10, 10), 10010);
+    EXPECT_EQ(Field::getIndex(0, 1), 1000000);
+    EXPECT_EQ(Field::getIndex(1, 1), 1000001);
+    EXPECT_EQ(Field::getIndex(10, 10), 10000010);
 }
 
 
@@ -58,9 +58,9 @@ TEST(FieldTest, GetPositionFromIndex) {
     
     EXPECT_EQ(Field::getPositionFromIndex(0), QPoint(0, 0));
     EXPECT_EQ(Field::getPositionFromIndex(1), QPoint(0, 1));
-    EXPECT_EQ(Field::getPositionFromIndex(1000), QPoint(1, 0));
-    EXPECT_EQ(Field::getPositionFromIndex(1001), QPoint(1, 1));
-    EXPECT_EQ(Field::getPositionFromIndex(10010), QPoint(10, 10));
+    EXPECT_EQ(Field::getPositionFromIndex(1000000), QPoint(1, 0));
+    EXPECT_EQ(Field::getPositionFromIndex(1000001), QPoint(1, 1));
+    EXPECT_EQ(Field::getPositionFromIndex(10000010), QPoint(10, 10));
 }
 
 
